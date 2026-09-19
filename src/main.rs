1d2ccb1ffa55e65d6476d3539a09e71756e96b87
Bogdan Olar <olar.bogdan.dev@gmail.com>
2026-09-20 00:27:56 +0300

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -123,74 +123,77 @@ async fn main(spawner: Spawner) {
     defmt::info!("EFM32XG Demo started!");
     defmt::info!("Press BTN0 (PF6) or BTN1 (PF7) to toggle LEDs");
 
-    let frames_in = FROM_SPI.receiver();
-    let frames_out = TO_SPI.sender();
+    // ---- Display Loop ----
+    {
+        let frames_in = FROM_SPI.receiver();
+        let frames_out = TO_SPI.sender();
 
-    let disp_frame = display::ls013b7dh03::take_display_frame();
-    frames_out.send(disp_frame).await;
+        frames_out
+            .send(display::ls013b7dh03::take_display_frame())
+            .await;
 
-    let mut color = false;
-    // Create styles used by the drawing operations.
-    let thin_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-    let border_stroke = PrimitiveStyleBuilder::new()
-        .stroke_color(BinaryColor::On)
-        .stroke_width(3)
-        .stroke_alignment(StrokeAlignment::Inside)
-        .build();
-    let character_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::Off);
-    let mut y_offset = 20;
+        // Create styles used by the drawing operations.
+        let thin_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
+        let border_stroke = PrimitiveStyleBuilder::new()
+            .stroke_color(BinaryColor::On)
+            .stroke_width(3)
+            .stroke_alignment(StrokeAlignment::Inside)
+            .build();
+        let character_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::Off);
+        let mut y_offset = 20;
 
-    let sec_duration: f32 = 1000.0;
-    let mut prev_frame_start = Instant::now();
-    loop {
-        let mut frame = frames_in.receive().await;
-        let frame_start = Instant::now();
-        let frame_duration = (frame_start - prev_frame_start).as_millis() as f32;
-        prev_frame_start = frame_start;
+        let sec_duration: f32 = 1000.0;
+        let mut prev_frame_start = Instant::now();
 
-        color = !color;
-        frame.clear(BinaryColor::Off);
+        loop {
+            let mut frame = frames_in.receive().await;
+            let frame_start = Instant::now();
+            let frame_duration = (frame_start - prev_frame_start).as_millis() as f32;
+            prev_frame_start = frame_start;
 
-        // Draw a 3px wide outline around the display.
-        frame
-            .bounding_box()
-            .into_styled(border_stroke)
+            frame.clear(BinaryColor::Off);
+
+            // Draw a 3px wide outline around the display.
+            frame
+                .bounding_box()
+                .into_styled(border_stroke)
+                .draw(&mut frame);
+
+            // Draw a triangle.
+            Triangle::new(
+                Point::new(100, 16 + y_offset),
+                Point::new(100 + 16, 16 + y_offset),
+                Point::new(100 + 8, y_offset),
+            )
+            .into_styled(thin_stroke)
             .draw(&mut frame);
+            y_offset = if y_offset == 0 {
+                DisplayFrame::HEIGHT as i32
+            } else {
+                y_offset - 1
+            };
 
-        // Draw a triangle.
-        Triangle::new(
-            Point::new(100, 16 + y_offset),
-            Point::new(100 + 16, 16 + y_offset),
-            Point::new(100 + 8, y_offset),
-        )
-        .into_styled(thin_stroke)
-        .draw(&mut frame);
-        y_offset = if y_offset == 0 {
-            DisplayFrame::HEIGHT as i32
-        } else {
-            y_offset - 1
-        };
+            // Draw FPS
+            let fps = if frame_duration.is_normal() {
+                sec_duration / frame_duration
+            } else {
+                0.0
+            };
+            if let Ok(fps_str) = format!(10; "{:.1} fps", fps) {
+                let fps_txt = Text::with_alignment(
+                    fps_str.as_str(),
+                    Point::new(0, 6),
+                    character_style,
+                    Alignment::Left,
+                );
+                frame.fill_solid(&fps_txt.bounding_box(), BinaryColor::On);
+                fps_txt.draw(&mut frame);
+            } else {
+                error!("Could not format fps {}", &fps);
+            }
 
-        // Draw FPS
-        let fps = if frame_duration.is_normal() {
-            sec_duration / frame_duration
-        } else {
-            0.0
-        };
-        if let Ok(fps_str) = format!(10; "{:.1} fps", fps) {
-            let fps_txt = Text::with_alignment(
-                fps_str.as_str(),
-                Point::new(0, 6),
-                character_style,
-                Alignment::Left,
-            );
-            frame.fill_solid(&fps_txt.bounding_box(), BinaryColor::On);
-            fps_txt.draw(&mut frame);
-        } else {
-            error!("Could not format fps {}", &fps);
+            frames_out.send(frame).await;
         }
-
-        frames_out.send(frame).await;
     }
 }
 
